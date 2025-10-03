52fcf635b655cb2340ed6731da7946b502801c0e
Olivia Rodrigue <orodrigue@itba.edu.ar>
2025-10-03 12:46:21 -0300

    testeo. Falta el include de las funciones

---
+++ Userland/SampleCodeModule/include/test_utils.h
@@ -0,0 +1,12 @@
+#ifndef TEST_UTILS_H
+#define TEST_UTILS_H
+#include <stdlib.h>
+#include <syscalls.h>
+
+uint32_t GetUint();
+uint32_t GetUniform(uint32_t max) ;
+uint8_t memcheck(void *start, uint8_t value, uint32_t size);
+int64_t satoi(char *str);
+
+
+#endif
---
+++ Userland/SampleCodeModule/tests/test_mm.c
@@ -0,0 +1,64 @@
+#include "../include/test_utils.h"
+
+#define MAX_BLOCKS 128
+
+typedef struct MM_rq {
+  void *address;
+  uint32_t size;
+} mm_rq;
+
+uint64_t test_mm(uint64_t argc, char *argv[]) {
+    mm_rq mm_rqs[MAX_BLOCKS];
+    uint8_t rq;
+    uint32_t total;
+    uint64_t max_memory;
+
+    if (argc != 1){
+        printf("ERROR: cantidad de argumentos\n");
+        return -1;
+    }
+
+    if(max_memory = satoi(argv[0]) <= 0){
+        printf("ERROR: argumento invalido\n");
+        return -1;
+    }
+    
+    printf("Iniciando test con maximo %d bytes\n", max_memory);
+    printf("Solicitando bloques de memoria...\n");
+    rq = 0;
+    total = 0;
+    while (rq < MAX_BLOCKS && total < max_memory) {
+        mm_rqs[rq].size = GetUniform(max_memory - total - 1) + 1;
+        mm_rqs[rq].address = malloc(mm_rqs[rq].size);
+
+        if (mm_rqs[rq].address != NULL) {
+            total += mm_rqs[rq].size; 
+            rq++;
+        }
+    }
+
+    printf("Inicializando bloques...\n");
+    uint32_t i;
+    for (i = 0; i < rq; i++) {
+        if (mm_rqs[i].address) {
+            memset(mm_rqs[i].address, i % 256, mm_rqs[i].size); 
+        }
+    }
+
+    printf("Chequeando bloques...\n");
+    for (i = 0; i < rq; i++) {
+        if (mm_rqs[i].address) {
+            if (!memcheck(mm_rqs[i].address, i , mm_rqs[i].size)) { 
+                return -1; 
+            }
+        }
+    }
+
+    printf("Liberando bloques...\n");
+    for (i = 0; i < rq; i++) {
+        if (mm_rqs[i].address) {
+            freeMem(mm_rqs[i].address); 
+        }
+    }
+    return 0;
+}
