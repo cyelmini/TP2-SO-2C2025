6e7c9e8d1d2662832b15c87a52f247b793b85de3
Mateo Lopez Badias <152566466+mlopezbadias@users.noreply.github.com>
2025-11-09 23:16:58 -0300

    rogarle a pvs studio q nos quiera
    Co-Authored-By: Olivia Rodrigue <138537751+OliviaRodrigue@users.noreply.github.com>
    Co-Authored-By: Catalina Yelmini <143967347+cyelmini@users.noreply.github.com>

--- Userland/SampleCodeModule/stdlib.c
+++ Userland/SampleCodeModule/stdlib.c
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 #include "include/stdlib.h"
 #include "include/stdio.h"
 #include "include/syscalls.h"
--- Userland/SampleCodeModule/tests/test_processes.c
+++ Userland/SampleCodeModule/tests/test_processes.c
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 #include "../include/syscalls.h"
 #include "../include/test_util.h"
 #include <stdio.h>
